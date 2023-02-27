18595e3e3a332ae0face4766a45455ad6b65b3d5
deeznanas1234 <125383768+deeznanas1234@users.noreply.github.com>
2023-02-27 10:27:27 -0600

    Update tokenadd.js

--- tokenadd.js
+++ tokenadd.js
@@ -1,10 +1,11 @@
-accname = prompt('Enter your blooket username');tokensamount = prompt('How many tokens do you want (max 500 daily)');fetch("https://api.blooket.com/api/users/addtokens", {
+accname = prompt('Enter your blooket username');tokensamount = prompt('How many tokens do you want (max 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999 daily)');fetch("https://api.blooket.com/api/users/addtokens", {
   "headers": {
     "accept": "application/json, text/plain, */*",
     "accept-language": "en-US,en;q=0.9",
     "authorization": localStorage.token,
     "content-type": "application/json;charset=UTF-8",
-    "sec-fetch-dest": "empty",
+    "sec-fetch-d
+     "est": "empty",
     "sec-fetch-mode": "cors",
     "sec-fetch-site": "same-site",
     "sec-gpc": "1"
